5f7671fc6eee3044cbb9da709df2378f4189a533
telingerErhard <93592012+tErhard@users.noreply.github.com>
2023-04-04 11:40:30 +0300

    Add files via upload

---
+++ C_sharp_2023/labs/lab_2/Controllers/AirportController.cs
@@ -0,0 +1,92 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace lab_1_pro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirportController : ControllerBase
+    {
+        private readonly AirportContext _context;
+
+        public AirportController(AirportContext context)
+        {
+            this._context = context;
+        }
+
+        // Read
+        [HttpGet]
+        public async Task<ActionResult<List<Airport>>> Get()
+        {
+            return Ok(await _context.Airport.ToListAsync());
+        }
+
+        // Read one
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Airport>> Get(int id)
+        {
+            var airport = await _context.Airport.FindAsync(id);
+            if (airport == null)
+            {
+                return BadRequest(404);
+            }
+            else
+            {
+                return Ok(airport);
+            }
+        }
+
+        // Add
+        [HttpPost]
+        public async Task<ActionResult<List<Airport>>> Add(Airport airport)
+        {
+            _context.Add(airport);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Airport.ToListAsync());
+        }
+
+        // Update
+        [HttpPut]
+        public async Task<ActionResult<List<Airport>>> Update(Airport request)
+        {
+            var airport = await _context.airport.FindAsync(request.Id);
+            if (airport == null)
+            {
+                return BadRequest(404);
+            }
+            else
+            {
+                airport.Number = request.Number;
+                airport.City = request.City;
+                airport.Seats = request.Seats;
+                airport.Time = request.Time;
+                airport.Price = request.Price;
+
+                await _context.SaveChangesAsync();
+
+                return Ok(airport);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Airport>>> Delete(int id)
+        {
+            var airport = await _context.Airport.FindAsync(id);
+            if (airport == null)
+            {
+                return BadRequest(404);
+            }
+            else
+            {
+                _context.Airport.Remove(airport);
+
+                await _context.SaveChangesAsync();
+
+                return Ok(airport);
+            }
+        }
+    }
+}
---
+++ C_sharp_2023/labs/lab_2/Models/Airport.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Airport
+    {
+        public int Id { get; set; }
+        public double  Number { get; set; }
+        public string? City { get; set; }
+        public double Seats { get; set; }
+        public string? Time { get; set; }
+        public double Price { get; set; }
+    }
+}
